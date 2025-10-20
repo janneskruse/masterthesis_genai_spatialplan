cbbdb925ec539ebb7abd188bbc2d988a05bc7cbf
janneskruse <janneskruse98@gmail.com>
2025-10-21 00:09:36 +0200

    add einops package

--- code/model/diffusion_blocks/unet_cond_base.py
+++ code/model/diffusion_blocks/unet_cond_base.py
@@ -2,8 +2,8 @@
 import torch
 from einops import einsum
 import torch.nn as nn
-from models.blocks import get_time_embedding
-from models.blocks import DownBlock, MidBlock, UpBlockUnet
+from diffusion_blocks.blocks import get_time_embedding
+from diffusion_blocks.blocks import DownBlock, MidBlock, UpBlockUnet
 from utils.config_utils import *
 
 
