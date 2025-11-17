ee309ec1ac4f0b1dac16bb38a818b62a2b11c32f
janneskruse <janneskruse98@gmail.com>
2025-11-17 06:07:25 +0100

    safe load modules for HPC cluster

--- code/model/utils/load_cuda.py
+++ code/model/utils/load_cuda.py
@@ -1,22 +1,48 @@
 import sys
 import os
-sys.path.append("/usr/share/lmod/lmod/init")
-from env_modules_python import module
+import torch
 
 def load_cuda():
+    
+    if torch.cuda.is_available():
+        print("✓ CUDA is available")
+    else:
+        print("Loading HPC cluster CUDA module...")
+    
+        try: 
+            sys.path.append("/usr/share/lmod/lmod/init")
+            from env_modules_python import module
+        
+            cuda_root = "/software/easybuild/el8/amd_zen3/all/CUDA/12.6.0"
 
-    cuda_root = "/software/easybuild/el8/amd_zen3/all/CUDA/12.6.0"
+            module("load", "CUDA")
 
-    module("load", "CUDA")
+            os.environ["PATH"] = f"{cuda_root}/bin:{os.environ['PATH']}"
+            os.environ["LD_LIBRARY_PATH"] = f"{cuda_root}/lib:{os.environ.get('LD_LIBRARY_PATH', '')}"
+            os.environ["CUDA_HOME"] = cuda_root
+            os.environ["CUDA_PATH"] = cuda_root
+            
+            if torch.cuda.is_available():
+                print("✓ CUDA is available now after loading module.")
+            else:
+                print("✗ CUDA is still not available after loading module.")
 
-    os.environ["PATH"] = f"{cuda_root}/bin:{os.environ['PATH']}"
-    os.environ["LD_LIBRARY_PATH"] = f"{cuda_root}/lib:{os.environ.get('LD_LIBRARY_PATH', '')}"
-    os.environ["CUDA_HOME"] = cuda_root
-    os.environ["CUDA_PATH"] = cuda_root
+        except Exception as e:
+            print("Error loading HPC cluster CUDA module:", e)
+            print("Proceeding without loading CUDA module.")
+        
     
     
 def load_gdal():
 
+    print("Loading HPC cluster GDAL module...")
+    try: 
+        sys.path.append("/usr/share/lmod/lmod/init")
+        from env_modules_python import module
+    except Exception as e:
+        print("Error importing env_modules_python:", e)
+        return
+    
     gdal_root = "/software/easybuild/el8/amd_zen3/all/GDAL/3.9.0-foss-2023b"
     module("load", "GDAL")
     
